b4545d3aafe6755ac3cf0680209e2afa0b18c576
Lewis <lewis.aedes@gmail.com>
2013-10-17 15:30:27 +0800

    init

---
+++ src/Lewis/Routing/Routing.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Lewis\Routing;
+
+class Routing
+{
+    public $config;
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    public function getController()
+    {
+        $request = $this->getRequest();
+        $match = $this->matchController($request['server']['PATH_INFO']);
+
+        return $this->headerController($match);
+    }
+
+    public function getRequest()
+    {
+        $request['get']     = $_GET;
+        $request['post']    = $_POST;
+        $request['file']    = $_FILES;
+        $request['session'] = $_SESSION;
+        $request['cookie']  = $_COOKIE;
+        $request['server']  = $_SERVER;
+        $request['request'] = $_REQUEST;
+
+        return $request;
+    }
+
+    public function matchController($requestPath)
+    {
+        foreach ($this->config as $config)
+        {
+            try {
+                $urlValue = $this->matchPath($requestPath, $config['path']);
+
+                return array("controller" => $config['call'],
+                             "urlValue"   => $urlValue);
+            } catch (\Exception $exc) {
+                continue;
+            }
+        }
+
+        return array("controller" => "404", "urlVal" => null);
+    }
+
+    public function matchPath($path, $config)
+    {
+        $path = explode("/", $path);
+        $config = explode("/", $config);
+        $returnValue = array();
+
+        foreach ($config as $key => $value) {
+            if(!isset($path[$key]))
+                throw new \Exception("path not match");
+
+            if($value == $path[$key])
+                continue;
+
+            if("\$" !== substr($value, 0, 1))
+                throw new \Exception("path not match");
+
+            $valName = str_replace("\$", "", $value);
+            $valContent = $path[$key];
+
+            $returnValue = array_merge($returnValue,
+                                       array($valName => $valContent));
+        }
+
+        return $returnValue;
+    }
+
+    public function headerController($config)
+    {
+        if("404" === $config["controller"]) {
+            header("HTTP/1.0 404 Not Found");
+            exit();
+        }
+
+        $call = explode("/", $config["controller"]);
+
+        $team       = $call[0];
+        $bundle     = $call[1];
+        $controller = $call[2];
+        $function   = $call[3];
+
+        $className = "\\$team\\$bundle\\$controller";
+
+        $class = new $className();
+
+        call_user_func_array(array($class, "setRequest"), array($this->getRequest()));
+        call_user_func_array(array($class, "setUrlValue"), array($config['urlValue']));
+
+        return call_user_func_array(array($class, $function), array());
+    }
+}
